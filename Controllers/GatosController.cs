4b9d960df8e2e8a2808a990a06f50696bcfe4ddd
raulbntzz <raul.benitezmulas24@campusfp.es>
2025-11-17 11:10:46 +0100

    Segundo ejercicio de gatos

---
+++ Controllers/GatosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace gatos.Controladores
+{
+    public class GatosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+
+        public IActionResult Persa()
+        {
+            return View("Persa");
+        }
+
+
+        public IActionResult Siames()
+        {
+            return View("Siames");
+        }
+
+
+        public IActionResult Bengali()
+        {
+            return View("Bengali");
+        }
+    }
+}
