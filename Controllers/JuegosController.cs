a7132a10851f404c1a87c1cb205a36be26a75114
raulbntzz <raul.benitezmulas24@campusfp.es>
2025-11-17 11:13:34 +0100

    Ejercicio sobre las categorías de los videojuegos

---
+++ Controllers/JuegosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Juegos.Controllers
+{
+    public class JuegosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Accion()
+        {
+            return View("Accion");
+        }
+
+        public IActionResult Aventura()
+        {
+            return View("Aventura");
+        }
+
+        public IActionResult Rol()
+        {
+            return View("Rol");
+        }
+
+        public IActionResult Estrategia()
+        {
+            return View("Estrategia");
+        }
+    }
+}
---
+++ Controllers/PlataformasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Juegos.Controllers
+{
+    public class PlataformasController : Controller
+    {
+        public IActionResult Consolas()
+        {
+            return View("Consolas");
+        }
+        public IActionResult Pc()
+        {
+            return View("Pc");
+        }
+        public IActionResult Cloud()
+        {
+            return View("Cloud");
+        }
+
+
+    }
+}
