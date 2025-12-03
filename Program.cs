9ea560ee915c91c6a807cb3927162a60cd64df11
raulbntzz <raul.benitezmulas24@campusfp.es>
2025-12-03 12:47:27 +0100

    Generación de números aleatorios en ficheros

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace FicherosAleatorios
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Task tarea = GenerarFicheroAsync();
+
+            while (!tarea.IsCompleted)
+            {
+                Console.WriteLine("Generación del fichero en proceso");
+                await Task.Delay(200);
+            }
+
+            Console.WriteLine("Finalizada la generación del fichero");
+        }
+
+        static async Task GenerarFicheroAsync()
+        {
+            await using var writer = new StreamWriter("numeros.txt");
+
+            var rnd = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int numero = rnd.Next(1,100);
+                await writer.WriteLineAsync(numero.ToString());
+                await writer.FlushAsync();
+                await Task.Delay(50);
+            }
+        }
+    }
+}
